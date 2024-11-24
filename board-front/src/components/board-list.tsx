55be9e2492d1a62002396bd5fe8eb6bb73e86cd8
potekichi <hrt_ioo1279@outlook.jp>
2024-11-24 14:07:31 +0900

    バックエンドと接続

--- board-front/src/components/board-list.tsx
+++ board-front/src/components/board-list.tsx
@@ -1,26 +1,38 @@
-
+import { useQuery } from "@tanstack/react-query";
 import Board from "./board";
 
-const POSTS = [
-  { id: 1, userName: "ポテきち", content: "おはよう\nヤッホー" },
-  { id: 2, userName: "ポテごん", content: "こんにちは" },
-  { id: 3, userName: "ぽてぽて", content: "こんばんは" },
-];
+type Post = {
+  id: string;
+  content: string;
+  createdById: string;
+  createdBy: string;
+};
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+const getPosts = async () => {
+  const posts: Post[] = await fetch(`${SERVER_URL}/posts`).then((res) =>
+    res.json()
+  );
+  console.log(posts);
+  return posts;
+};
 
 const BoardList = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
   return (
     <div>
       <h2 className="font-bold text-xl text-center mb-2">みんなの投稿</h2>
-      <div className="flex flex-col gap-3">
-        {POSTS.map((post) => (
-          <Board
-            key={post.id}
-            id={post.id}
-            userName={post.userName}
-            content={post.content}
-          />
-        ))}
-      </div>
+      {isLoading && <p className="text-center">Loading...</p>}
+      {data && (
+        <div className="flex flex-col gap-3">
+          {data.map((post: Post) => (
+            <Board post={post} key={post.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- board-front/src/components/board.tsx
+++ board-front/src/components/board.tsx
@@ -1,24 +1,26 @@
 import { FaHeart } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { FaShareFromSquare } from "react-icons/fa6";
-type Post = {
-  id: number;
-  userName: string;
+
+type PostProps = {
+  id: string;
   content: string;
+  createdById: string;
+  createdBy: string
 };
 
-const Board = ({ id, userName, content }: Post) => {
-  const deletePost = (id: number) => {
+const Board = ( {post}:{post:PostProps} ) => {
+  const deletePost = (id: string) => {
     console.log(id);
   };
   return (
     <div className="border w-96 pl-5 mx-auto bg-slate-50 rounded py-2 px-3">
       <div className="text-left">
-        <span>{userName}</span>
+        <span>{post.createdBy}</span>
         <span>2024/12/7 12:59</span>
       </div>
 
-      <p className="whitespace-pre-line text-left">{content}</p>
+      <p className="whitespace-pre-line text-left">{post.content}</p>
       <div className="flex gap-5 justify-end">
         <button className="">
           {/* <FaRegHeart /> */}
@@ -28,7 +30,7 @@ const Board = ({ id, userName, content }: Post) => {
           <FaShareFromSquare size={20} />
         </button>
         <button
-          onClick={() => deletePost(id)}
+          onClick={() => deletePost(post.id)}
           className="text-red-400"
         >
           <MdDelete size={25}/>
